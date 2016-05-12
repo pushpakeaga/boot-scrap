f865937d0f33b80c2a867697b1653bbfc56df2eb
pushpakeaga <pushpak.eaga@gmail.com>
2016-05-12 17:30:28 -0400

    Create params.pp

---
+++ puppet/modules/allpuppetcode/params.pp
@@ -0,0 +1,14 @@
+class httpd::params {
+$package = ""
+$package_ensure = ""
+$config_file = ""
+$file_ensure = ""
+$file_source = ""
+$service = ""
+$service_ensure = ""
+$service_enable = ""
+$file_content = ""
+$Listen = '80'
+$ServerTokens = 'OS'
+
+}
